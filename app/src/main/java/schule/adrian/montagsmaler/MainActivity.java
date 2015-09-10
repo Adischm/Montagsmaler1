3b0464e2b5ecad254d70aace9b429fb1a5893ce6
fabundi <fabiundi@gmx.de>
2015-09-10 14:06:53 +0200

    Neue Klassen: Controller, Lobby; Neue Act: Lobby

--- app/src/main/java/schule/adrian/montagsmaler/MainActivity.java
+++ app/src/main/java/schule/adrian/montagsmaler/MainActivity.java
@@ -167,8 +167,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //HttpPost post = new HttpPost("http://postcatcher.in/catchers/55f160782dea750300000518");
 
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-        params.add(new BasicNameValuePair("user", user));
-        params.add(new BasicNameValuePair("pass", pass));
+        params.add(new BasicNameValuePair("Benutzername", user));
+        params.add(new BasicNameValuePair("Passwort", pass));
         params.add(new BasicNameValuePair("format", format));
         params.add(new BasicNameValuePair("method", method));
 
@@ -205,7 +205,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void run() {
             try {
                 //Your code goes here
-                requestGetTest();
+                //requestGetTest();
+                requestPostTest();
             } catch (Exception e) {
                 e.printStackTrace();
             }
